b67f2cec5a2d144dc12c55bc85c7ad291e40a10b
Nolan Waite <nwaite@antelope-temp.als.local>
2008-07-09 13:42:20 -0600

    Added previously untracked files.

---
+++ src/Network Classes/AQConnectionController.h
@@ -0,0 +1,45 @@
+// AQConnectionController.h
+// ConnectionController manages a single AsyncSocket instance and keeps its associated ConnectionArrayController apprised of its status.
+//
+// Created May 27, 2008 by nwaite
+
+#import "AsyncSocket.h"
+
+@interface AQConnectionController : NSObject
+{
+	AsyncSocket	*_socket;
+	id			_arrayController;
+	id			_associatedObject;
+	NSError		*_error;
+	BOOL		_handshakeComplete;
+	BOOL		_haveSeenFirstLMDirectives;
+	id			_objectRequestingGameListUpdate;
+}
+
+- (id)initWithHost:(NSString *)host port:(UInt16)port for:(id)sender arrayController:(id)arrayController;
+- (void)dealloc;
+
+// Accessors/setters/etc.
+- (id)associatedObject;
+- (NSError *)error;
+- (BOOL)isServerConnection;
+- (void)close;
+- (BOOL)isConnected;
+- (NSString *)connectedHostOrIPAddress;
+
+// Boom
+- (void)leaveGame:(id)sender;
+- (void)disconnectFromServer:(id)sender;
+
+// Sending some outgoing mail
+- (void)outgoingLobbyMessage:(NSString *)lobbyMessage;
+- (void)updateGameListFor:(id)anObject;
+- (void)retryUpdateGameList:(NSTimer *)aTimer;
+
+// AsyncSocket delegate selectors
+- (void)onSocket:(AsyncSocket *)socket willDisconnectWithError:(NSError *)err;
+- (void)onSocketDidDisconnect:(AsyncSocket *)socket;
+- (void)onSocket:(AsyncSocket *)socket didConnectToHost:(NSString *)host port:(UInt16)port;
+- (void)onSocket:(AsyncSocket *)socket didReadData:(NSData *)data withTag:(long)tag;
+- (void)onSocket:(AsyncSocket *)socket didWriteDataWithTag:(long)tag;
+@end
---
+++ src/Network Classes/AQConnectionController.m
@@ -0,0 +1,365 @@
+// AQConnectionController.m
+//
+// Created May 27, 2008 by nwaite
+
+#import "AQConnectionController.h"
+#import "AQConnectionArrayController.h"
+#import "AQAcquireController.h"
+#import "AQNetacquireDirective.h"
+
+@interface AQConnectionController (Private)
+// Netacquire directive handling
+// Incoming directives
+- (void)_receivedDirective:(AQNetacquireDirective *)directive;
+- (void)_receivedLMDirective:(AQNetacquireDirective *)lobbyMessageDirective;
+- (void)_receivedFirstLMDirectives:(AQNetacquireDirective *)bunchOfLMDirectives;
+- (void)_receivedGameListDirective:(AQNetacquireDirective *)gameListDirective;
+- (NSArray *)_parseMultipleLMDirectives:(AQNetacquireDirective *)multipleLMDirectives;
+- (void)_receivedMDirective:(AQNetacquireDirective *)messageDirective;
+- (void)_receivedSPDirective:(AQNetacquireDirective *)startPlayerDirective;
+
+// And outgoing directives
+- (void)_sendDirectiveData:(NSData *)data;
+- (void)_sendDirectiveWithCode:(NSString *)directiveCode;
+- (void)_sendBMDirectiveWithMessage:(NSString *)message;
+- (void)_sendPLDirectiveWithDisplayName:(NSString *)displayName versionStrings:(NSArray *)versionStrings;
+
+// And their supporting cast
+- (void)_incomingLobbyMessage:(NSString *)lobbyMessage;
+@end
+
+@implementation AQConnectionController
+- (id)initWithHost:(NSString *)host port:(UInt16)port for:(id)sender arrayController:(id)arrayController;
+{
+	if (![super init])
+		return nil;
+	
+	if ([host length] == 0)
+		return nil;
+	
+	if (sender == nil || arrayController == nil)
+		return nil;
+	
+	_associatedObject = [sender retain];
+	_arrayController = [arrayController retain];
+	_error = [NSError alloc];
+	_handshakeComplete = NO;
+	_haveSeenFirstLMDirectives = NO;
+	_objectRequestingGameListUpdate = nil;
+	
+	_socket = [[AsyncSocket alloc] initWithDelegate:self];
+	
+	if (![_socket connectToHost:host onPort:port error:&_error])
+		if ([_associatedObject respondsToSelector:@selector(connection:willDisconnectWithError:)])
+			[_associatedObject connection:self willDisconnectWithError:_error];
+
+	return self;
+}
+
+- (void)dealloc;
+{
+	[_associatedObject release];
+	[_arrayController release];
+	[_socket release];
+	[_error release];
+	
+	[super dealloc];
+}
+
+
+// Accessors/setters/etc.
+- (id)associatedObject;
+{
+	return _associatedObject;
+}
+
+- (NSError *)error;
+{
+	return _error;
+}
+
+- (BOOL)isServerConnection;
+{
+	return ([(AQConnectionArrayController *)_arrayController serverConnection] == self);
+}
+
+- (void)close;
+{
+	[_socket disconnectAfterWriting];
+}
+
+- (BOOL)isConnected;
+{
+	return [_socket isConnected];
+}
+
+- (NSString *)connectedHostOrIPAddress;
+{
+	return [_socket connectedHost];
+}
+
+
+// Boom
+- (void)leaveGame:(id)sender;
+{
+//  Uncomment when we're actually joining games
+//	[self _sendDirectiveWithCode:@"LV"];
+}
+
+- (void)disconnectFromServer:(id)sender;
+{
+//	[self _sendDirectiveWithCode:@"CL"];
+	[self close];
+}
+
+
+// Sending some outgoing mail
+- (void)outgoingLobbyMessage:(NSString *)lobbyMessage;
+{
+	if ([lobbyMessage length] == 0)
+		return;
+	
+	[self _sendBMDirectiveWithMessage:lobbyMessage];
+}
+
+- (void)updateGameListFor:(id)anObject;
+{
+	if (!_handshakeComplete) {
+		[NSTimer scheduledTimerWithTimeInterval:0.2 target:self selector:@selector(retryUpdateGameList:) userInfo:anObject repeats:NO];
+		return;
+	}
+	
+	_objectRequestingGameListUpdate = anObject;
+	[self _sendDirectiveWithCode:@"LG"];
+}
+
+- (void)retryUpdateGameList:(NSTimer *)aTimer;
+{
+	[self updateGameListFor:[aTimer userInfo]];
+}
+
+
+// AsyncSocket delegate selectors
+- (void)onSocket:(AsyncSocket *)socket willDisconnectWithError:(NSError *)err;
+{
+	[_error release];
+	_error = [err retain];
+	
+	if ([_associatedObject respondsToSelector:@selector(connection:willDisconnectWithError:)])
+		[_associatedObject connection:self willDisconnectWithError:err];
+}
+
+- (void)onSocketDidDisconnect:(AsyncSocket *)socket;
+{
+	[(AQConnectionArrayController *)_arrayController connectionClosed:self];
+}
+
+- (void)onSocket:(AsyncSocket *)socket didConnectToHost:(NSString *)host port:(UInt16)port;
+{
+	if (socket != _socket)
+		return;
+	
+	if ([self isServerConnection])
+		[_socket readDataWithTimeout:-1 tag:0];
+}
+
+- (void)onSocket:(AsyncSocket *)socket didReadData:(NSData *)data withTag:(long)tag;
+{
+	[self _receivedDirective:[AQNetacquireDirective directiveWithData:data]];
+	[_socket readDataWithTimeout:-1 tag:0];
+}
+
+- (void)onSocket:(AsyncSocket *)socket didWriteDataWithTag:(long)tag;
+{
+	[_socket readDataWithTimeout:-1 tag:0];
+}
+@end
+
+@implementation AQConnectionController (Private)
+// Netacquire directive handling
+// Incoming directives
+- (void)_receivedDirective:(AQNetacquireDirective *)directive;
+{
+	if ([[directive directiveCode] isEqualToString:@"LM"]) {
+		if (_haveSeenFirstLMDirectives)
+			[self _receivedLMDirective:directive];
+		else {
+			[self _receivedFirstLMDirectives:directive];
+			_haveSeenFirstLMDirectives = YES;
+		}
+		
+		return;
+	}
+	
+	if ([[directive directiveCode] isEqualToString:@"M"]) {
+		[self _receivedMDirective:directive];
+		return;
+	}
+	
+	if ([[directive directiveCode] isEqualToString:@"SP"]) {
+		[self _receivedSPDirective:directive];
+		return;
+	}
+}
+
+- (void)_receivedLMDirective:(AQNetacquireDirective *)lobbyMessageDirective;
+{
+	// The server's response to LG and LU directives come in chunks of LM directives.
+	NSString *messageText = [[lobbyMessageDirective parameters] objectAtIndex:0];
+	if ([[messageText substringToIndex:15] isEqualToString:@"\"# Active games"]) {
+		[self _receivedGameListDirective:lobbyMessageDirective];
+		return;
+	}
+	
+	if ([messageText characterAtIndex:1] == '*') {
+		NSArray *messages = [self _parseMultipleLMDirectives:lobbyMessageDirective];
+		NSEnumerator *messageEnumerator = [messages objectEnumerator];
+		id curMessage;
+		while (curMessage = [messageEnumerator nextObject])
+			[self _incomingLobbyMessage:[curMessage substringWithRange:NSMakeRange(1, [curMessage length] - 2)]];
+		return;
+	}
+	
+	[self _incomingLobbyMessage:[messageText substringWithRange:NSMakeRange(1, [messageText length] - 2)]];
+}
+
+- (void)_receivedFirstLMDirectives:(AQNetacquireDirective *)bunchOfLMDirectives;
+{
+	if ([_associatedObject respondsToSelector:@selector(connectedToServer)])
+		[_associatedObject connectedToServer];
+	
+	NSString *bunchOfLMDirectivesString = [[bunchOfLMDirectives parameters] objectAtIndex:0];
+	bunchOfLMDirectivesString = [bunchOfLMDirectivesString substringFromIndex:1];
+	NSRange firstLobbyMessageRange = [bunchOfLMDirectivesString rangeOfString:@"\""];
+	firstLobbyMessageRange.length = firstLobbyMessageRange.location - 1;
+	firstLobbyMessageRange.location = 0;
+	
+	[self _incomingLobbyMessage:[bunchOfLMDirectivesString substringWithRange:firstLobbyMessageRange]];
+	
+	NSRange secondLobbyMessageRange = [bunchOfLMDirectivesString rangeOfString:@"LM"];
+	secondLobbyMessageRange.location += 4;
+	secondLobbyMessageRange.length = [bunchOfLMDirectivesString length] - secondLobbyMessageRange.location - 1;
+	bunchOfLMDirectivesString = [bunchOfLMDirectivesString substringWithRange:secondLobbyMessageRange];
+	
+	secondLobbyMessageRange = [bunchOfLMDirectivesString rangeOfString:@"\""];
+	secondLobbyMessageRange.length = secondLobbyMessageRange.location - 1;
+	secondLobbyMessageRange.location = 0;
+	
+	_handshakeComplete = YES;
+	[self _incomingLobbyMessage:[bunchOfLMDirectivesString substringWithRange:secondLobbyMessageRange]];
+}
+
+- (void)_receivedGameListDirective:(AQNetacquireDirective *)gameListDirective;
+{
+	if (_objectRequestingGameListUpdate == nil)
+		return;
+	
+	NSString *gameListString = [[gameListDirective parameters] objectAtIndex:0];
+	NSMutableArray *gameList = [[[NSMutableArray alloc] initWithCapacity:5] autorelease];
+	NSRange gameListStartRange = NSMakeRange(0, 1);	
+	NSRange gameListEndRange = NSMakeRange(1, 1);
+	while (gameListStartRange.location != gameListEndRange.location) {
+		NSRange gameListStartRange = [gameListString rangeOfString:@";:LM;"];
+		if (gameListStartRange.location == NSNotFound)
+			break;
+		NSRange gameListRange = NSMakeRange(gameListStartRange.location, [gameListString length] - gameListStartRange.location);
+		gameListString = [gameListString substringWithRange:gameListRange];
+		if ([gameListString length] < 13)
+			break;
+	
+		gameListString = [gameListString substringFromIndex:12];
+		NSRange gameNumberStartRange = [gameListString rangeOfString:@"#"];
+		NSRange gameNumberEndRange = [gameListString rangeOfString:@"->"];
+		if (gameNumberStartRange.location == NSNotFound || gameNumberEndRange.location == NSNotFound)
+			break;
+		NSRange gameNumberRange = NSMakeRange(gameNumberStartRange.location + 1, gameNumberEndRange.location - gameNumberStartRange.location - 1);
+		[gameList addObject:[gameListString substringWithRange:gameNumberRange]];
+	}
+	
+	if ([_objectRequestingGameListUpdate respondsToSelector:@selector(updatedGameList:)]) {
+		[_objectRequestingGameListUpdate updatedGameList:gameList];
+	}
+	
+	_objectRequestingGameListUpdate = nil;
+}
+
+- (NSArray *)_parseMultipleLMDirectives:(AQNetacquireDirective *)multipleLMDirectives;
+{
+	NSString *messages = [[multipleLMDirectives parameters] objectAtIndex:0];
+	NSMutableArray *separatedMessages = [NSMutableArray arrayWithCapacity:4];
+	
+	NSRange endOfFirstMessage = [messages rangeOfString:@";:LM;"];
+	while (endOfFirstMessage.location != NSNotFound) {
+		endOfFirstMessage.length = endOfFirstMessage.location;
+		endOfFirstMessage.location = 0;
+		[separatedMessages addObject:[messages substringWithRange:endOfFirstMessage]];
+		messages = [messages substringFromIndex:(endOfFirstMessage.length + 5)];
+		endOfFirstMessage = [messages rangeOfString:@";:LM;"];
+	}
+	[separatedMessages addObject:messages];
+	
+	return separatedMessages;
+}
+
+- (void)_receivedMDirective:(AQNetacquireDirective *)messageDirective;
+{
+	if ([[messageDirective parameters] count] != 1)
+		return;
+	
+	if ([[[[messageDirective parameters] objectAtIndex:0] substringToIndex:26] isEqualToString:@"\"E;Duplicate user Nickname"])
+		if ([_associatedObject respondsToSelector:@selector(displayNameAlreadyInUse)])
+			[_associatedObject displayNameAlreadyInUse];
+}
+
+- (void)_receivedSPDirective:(AQNetacquireDirective *)startPlayerDirective;
+{
+	if ([[startPlayerDirective parameters] count] == 0) {
+		NSLog(@"%s version string was empty!", _cmd);
+		return;
+	}
+	
+	NSRange versionInfoRange = NSMakeRange(0, [[startPlayerDirective parameters] count] - 1);
+	[self _sendPLDirectiveWithDisplayName:[(AQAcquireController *)_associatedObject localPlayersName] versionStrings:[[startPlayerDirective parameters] subarrayWithRange:versionInfoRange]];
+}
+
+
+
+// And outgoing directives
+- (void)_sendDirectiveData:(NSData *)data;
+{
+	[_socket writeData:data withTimeout:20.0 tag:0];
+}
+
+- (void)_sendDirectiveWithCode:(NSString *)directiveCode;
+{
+	AQNetacquireDirective *directive = [[[AQNetacquireDirective alloc] init] autorelease];
+	[directive setDirectiveCode:directiveCode];
+	[self _sendDirectiveData:[directive protocolData]];
+}
+
+- (void)_sendBMDirectiveWithMessage:(NSString *)message;
+{
+	AQNetacquireDirective *directive = [[[AQNetacquireDirective alloc] init] autorelease];
+	[directive setDirectiveCode:@"BM"];
+	[directive addParameter:@"Lobby"];
+	[directive addParameter:[NSString stringWithFormat:@"\"%@\"", message]];
+	[self _sendDirectiveData:[directive protocolData]];
+}
+
+- (void)_sendPLDirectiveWithDisplayName:(NSString *)displayName versionStrings:(NSArray *)versionStrings;
+{
+	AQNetacquireDirective *directive = [[[AQNetacquireDirective alloc] init] autorelease];
+	[directive setDirectiveCode:@"PL"];
+	[directive addParameter:displayName];
+	[directive addParameters:versionStrings];
+	[self _sendDirectiveData:[directive protocolData]];
+}
+
+
+// And their supporting cast
+- (void)_incomingLobbyMessage:(NSString *)lobbyMessage;
+{
+	if ([_associatedObject respondsToSelector:@selector(incomingLobbyMessage:)])
+		[_associatedObject incomingLobbyMessage:lobbyMessage];
+}
+@end
